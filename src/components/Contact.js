db78b1b1e6dab4e44c00f692a7ea32a55879b9c2
fsiqueirag <fabrizio.siqueira@hotmail.com>
2020-11-25 19:43:30 -0300

    corregir contacto

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,8 @@ export const Contact = () => {
     return (
         <section className="contact__section" id="contact">
             <h2>Contacto</h2>
-            <p>+54 1155057883</p>
-            <p>antonio@gmail.com</p>
+            <p>+54 9 3515 39-3021</p>
+            <p>toniorosa@hotmail.com</p>
         </section>
     )
 }
