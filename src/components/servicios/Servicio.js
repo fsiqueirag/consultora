e7c01e7bd03ba934ee0bc2ea88deed200cd5de0a
fsiqueirag <fabrizio.siqueira.g@gmail.com>
2020-12-20 11:44:56 -0300

    cambiar servicios y redes

--- src/components/servicios/Servicio.js
+++ src/components/servicios/Servicio.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
 
 export const Servicio = ({ title, icon, content1, content2, content3, contentBold }) => {
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(!isClicked);
+    }
+
     return (
         <div className="servicios__card-container col-md-6">
             <ScrollAnimation
@@ -9,15 +16,30 @@ export const Servicio = ({ title, icon, content1, content2, content3, contentBol
                 animateOnce={true}
                 className="servicios__scrollContainer"
             >
-                <div className="servicios__card">
+                <div className={`servicios__card ${isClicked && 'servicios__card-full'}`}>
                     <span className={`${icon} servicios__icon`} />
                     <h3>{title}</h3>
                     <p>{content1}</p>
-                    <br />
-                    <p>{content2}</p>
-                    <br />
-                    <p>{content3}</p>
-                    <p className="servicios__content-bold">{contentBold}</p>
+
+                    {
+                        isClicked
+                        ?
+                        <ScrollAnimation 
+                        animateIn="animate__animated animate__fadeIn"
+                        animateOnce={true}
+                        >
+                            <br />
+                            <p>{content2}</p>
+                            <br />
+                            <p>{content3}</p>
+                            <p className="servicios__content-bold">{contentBold}</p>
+                    <button className="btn btn-dark servicios__full-btn" onClick={handleClick}>Ver menos</button>
+                        </ScrollAnimation>
+                        :
+                    <button className="btn btn-outline-dark servicios__small-btn" onClick={handleClick}>Ver más</button>
+
+                    }
+                    
                 </div>
             </ScrollAnimation>
         </div>
